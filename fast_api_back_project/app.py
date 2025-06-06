361578d8a82c6ef3148874f6c3d5dd89d0036597
Luis Carlos Bezera <luiscarlossilvabezerra@gmail.com>
2025-06-06 16:09:57 -0300

    feat: add new endpoint to html message

--- fast_api_back_project/app.py
+++ fast_api_back_project/app.py
@@ -1,8 +1,26 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_api_back_project.schema import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get('/html_message', response_class=HTMLResponse)
+def read_root_html():
+    return """
+    <html>
+      <head>
+        <title>Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1>Olá mundo!</h1>
+      </body>
+    </html>"""
