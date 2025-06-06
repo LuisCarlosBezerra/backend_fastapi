2e67d13cbc3efda11afe3f14aca261204b491d60
Luis Carlos Bezera <luiscarlossilvabezerra@gmail.com>
2025-06-06 16:11:03 -0300

    test: created test for new endpoint

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,10 @@ def test_read_root_retorna_ok_e_ola_mundo():
     response = client.get('/')  # act
     assert response.status_code == HTTPStatus.OK  # assert
     assert response.json() == {'message': 'Olá mundo!'}
+
+
+def test_read_root_retorna_ok_e_ola_mundo_html():
+    client = TestClient(app)
+    response = client.get('/html_message')
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Olá mundo!</h1>' in response.text
